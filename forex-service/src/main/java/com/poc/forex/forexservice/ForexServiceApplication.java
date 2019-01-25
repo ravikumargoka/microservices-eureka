0400af72612b5a4723b1839e3896ca8db0448347
ravikumargoka <ravikumargoka@gmail.com>
2019-01-25 16:37:23 +0530

    first commit

---
+++ forex-service/src/main/java/com/poc/forex/forexservice/ForexServiceApplication.java
@@ -0,0 +1,16 @@
+package com.poc.forex.forexservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ForexServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForexServiceApplication.class, args);
+	}
+
+}
+
---
+++ src/main/java/com/poc/eureka/microserviceseureka/MicroservicesEurekaApplication.java
@@ -0,0 +1,14 @@
+package com.poc.eureka.microserviceseureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesEurekaApplication.class, args);
+	}
+
+}
+
